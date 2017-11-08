d18bbaedb5fb6e15819127dc6b5492389eba14f7
PedroRodP <ppedro.rod@gmail.com>
2017-11-08 17:52:43 -0300

    SantaFe ya puede ser comprada si no tiene dueño y arroja ExcepcionTerrenoOcupado si ya tiene dueño

--- test/unitarios/SantaFeTest.java
+++ test/unitarios/SantaFeTest.java
@@ -14,16 +14,25 @@ public class SantaFeTest {
 	private static final double DELTA = 1e-15;
 
 	@Test
-	public void test01ComprarTerrenoNoOcupadoDescuentaPrecioCorrectoDelCapitalDelJugador() {
+	public void test01ComprarTerrenoNoOcupadoDescuentaPrecioCorrectoDelCapitalDelJugador() throws ExcepcionTerrenoOcupado {
 		
 		Jugador jugador = new Jugador();
 		Barrio barrio = new SantaFe();
 		
-		try { 
-			barrio.comprarTerreno(jugador);
-		}
-		catch (ExcepcionTerrenoOcupado e) {}
+		barrio.comprarTerreno(jugador);
 		
 		Assert.assertEquals(15000, 100000 - jugador.balance(), DELTA);
+	}
+	
+	@Test (expected = ExcepcionTerrenoOcupado.class)
+	public void test02ComprarTerrenoOcupadoArrojaExcepcion() throws ExcepcionTerrenoOcupado {
+		
+		Jugador jugador1 = new Jugador();
+		Jugador jugador2 = new Jugador();
+		Barrio barrio = new SantaFe();
+		
+		barrio.comprarTerreno(jugador1);
+		barrio.comprarTerreno(jugador2);
+		
 	}
 }
