202ef2d06916684a96f63b9737d2045de558e4b9
NicoCometta <nico_cometta@hotmail.com>
2017-11-20 21:17:14 -0300

    Prueba 15 de segunda entrega

---
+++ test/unitarios/ImpuestoTest.java
@@ -0,0 +1,28 @@
+package unitarios;
+
+import modelo.Jugador;
+import modelo.casilleros.Impuesto;
+import modelo.excepciones.ExcepcionCapitalInsuficiente;
+import modelo.excepciones.ExcepcionJugadorPreso;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by nico on 20/11/17.
+ */
+public class ImpuestoTest {
+
+    private static final double DELTA = 1e-15;
+
+    @Test
+    public void test01UnJugadorCaeImpuestoAlLujoYSuCapitalSeDecrementaEn10Porciento() throws ExcepcionJugadorPreso, ExcepcionCapitalInsuficiente {
+        Jugador unJugador = new Jugador();
+        Impuesto casilleroImpuesto= new Impuesto();
+
+        double capitalAnterior = unJugador.getCapital();
+
+        casilleroImpuesto.caer(unJugador, 1);
+
+        Assert.assertEquals(capitalAnterior * 0.9, unJugador.getCapital(), DELTA);
+    }
+}
